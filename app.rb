73f998953a8bf87e67183c14327a5bad64f80c63
Jun Lin <linjunpop@gmail.com>
2014-03-24 16:59:16 +0800

    Convert base64 on client side.

--- app.rb
+++ app.rb
@@ -1,16 +1,14 @@
 require "bundler/setup"
 
-require "base64"
 require 'sinatra'
 require 'typhoeus'
 
 post '/' do
   request.body.rewind  # in case someone already read it
-  receipt = Base64.encode64(request.body.read)
   request = Typhoeus::Request.new(
     "https://buy.itunes.apple.com/verifyReceipt",
     method: :post,
-    params: { "receipt-data" => receipt }
+    params: { "receipt-data" => request.body.read }
   )
   request.run.body
 end
