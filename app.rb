6571e8597237f52ade9f0723b9e58238611c2640
Jun Lin <linjunpop@gmail.com>
2014-03-24 17:18:55 +0800

    Fix duplicated variable name.

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ require 'typhoeus'
 
 post '/' do
   request.body.rewind  # in case someone already read it
-  request = Typhoeus::Request.new(
+  external_request = Typhoeus::Request.new(
     "https://sandbox.itunes.apple.com/verifyReceipt",
     method: :post,
     params: { "receipt-data" => request.body.read }
   )
-  request.run.body
+  external_request.run.body
 end
