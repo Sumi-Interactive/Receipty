6115c99507c1f88d984a81fe502747743346e707
Jun Lin <linjunpop@gmail.com>
2014-04-08 13:41:42 +0800

    Change Recipe verification API to production.

--- app.rb
+++ app.rb
@@ -4,10 +4,11 @@ require 'sinatra'
 require 'typhoeus'
 
 post '/' do
+  api_url = "https://buy.itunes.apple.com/verifyReceipt"
   request.body.rewind  # in case someone already read it
   receipt = request.body.read
   external_request = Typhoeus::Request.new(
-    "https://sandbox.itunes.apple.com/verifyReceipt",
+    api_url,
     method: :post,
     body: receipt
   )
